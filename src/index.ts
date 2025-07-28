b314b498b177d1b966a02ec09d04b76f08f9bb61
Ritesh <119659853+rit3zh@users.noreply.github.com>
2025-07-28 10:39:24 -0400

    Added README

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,2 @@
-// Reexport the native module. On web, it will be resolved to ExpoiOSAnimatedMeshGradientModule.web.ts
-// and on native platforms to ExpoiOSAnimatedMeshGradientModule.ts
-export { default } from './ExpoiOSAnimatedMeshGradientModule';
-export { default as ExpoiOSAnimatedMeshGradientView } from './ExpoiOSAnimatedMeshGradientView';
-export * from  './ExpoiOSAnimatedMeshGradient.types';
+export * from "./AnimatedMeshGradient.ios";
+export * from "./types";
